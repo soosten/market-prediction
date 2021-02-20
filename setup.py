6a166fb9f73c87f0f163ea12ec8f6af3199c8da6
soosten <psgithub@protonmail.com>
2021-02-21 00:23:31 +0100

    Final ensemble setup

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 import json
 import os
 import subprocess
-import tempfile
 import time
 
 
@@ -12,66 +11,99 @@ def main():
     # path to the repository from working directory
     path = os.curdir
 
-    # choose an operation
-    push_eda_kernel(path, user)
-    # push_data(path, user)
-    # push_tpu_kernel(path, user)
-    # push_utility_kernel(path, user)
-    # push_submission_kernel(path, user)
+    # EDA
+    # push_eda(path, user)
+
+    # models
+    push_nn(path, user)
+    wait_for_kernel(user, "jane-street-nn")
+
+    push_edaxgb(path, user)
+    wait_for_kernel(user, "jane-street-eda-xgb")
+
+    push_plsxgb(path, user)
+    wait_for_kernel(user, "jane-street-pls-xgb")
+
+    # tuner
+    # push_tuner(path, user)
+    # wait_for_kernel(user, "jane-street-tuner")
+
+    # submission
+    push_submission(path, user)
 
     return
 
 
-def push_data(path, user):
-    kernel = "jane-street-conversion"
-
-    metadata = {"id": user + "/" + kernel,
-                "title": "Jane Street - Conversion",
-                "code_file": "conversion.ipynb",
+def push_eda(path, user):
+    metadata = {"id": user + "/jane-street-eda",
+                "title": "Jane Street - EDA",
+                "code_file": "eda.ipynb",
                 "language": "python",
                 "kernel_type": "notebook",
                 "competition_sources": ["jane-street-market-prediction"]}
 
     push_kernel(path, metadata)
-
-    wait_for_kernel(user, kernel)
-
-    metadata = {"id": user + "/jane-street-data",
-                "title": "Jane Street - Data",
-                "licenses": [{"name": "CC-BY-SA-4.0"}]}
-
-    kernel_to_dataset(user, kernel, metadata)
     return
 
 
-def push_tpu_kernel(path, user):
-    metadata = {"id": user + "/jane-street-tpu",
-                "title": "Jane Street - TPU",
-                "code_file": "tpu.ipynb",
+def push_nn(path, user):
+    metadata = {"id": user + "/jane-street-nn",
+                "title": "Jane Street - NN",
+                "code_file": "nn.ipynb",
                 "language": "python",
                 "kernel_type": "notebook",
-                "enable_internet": "true",
-                "dataset_sources": [user + "/jane-street-data"]}
+                "enable_gpu": "true",
+                "competition_sources": ["jane-street-market-prediction"]}
 
     push_kernel(path, metadata)
     return
 
 
-def push_utility_kernel(path, user):
-    metadata = {"id": user + "/jane-street-utility",
-                "title": "Jane Street - Utility",
-                "code_file": "utility.ipynb",
+def push_edaxgb(path, user):
+    metadata = {"id": user + "/jane-street-eda-xgb",
+                "title": "Jane Street - EDA XGB",
+                "code_file": "eda-xgb.ipynb",
+                "language": "python",
+                "kernel_type": "notebook",
+                "enable_gpu": "true",
+                "enable_internet": "true",
+                "competition_sources": ["jane-street-market-prediction"]}
+
+    push_kernel(path, metadata)
+    return
+
+
+def push_plsxgb(path, user):
+    metadata = {"id": user + "/jane-street-pls-xgb",
+                "title": "Jane Street - PLS XGB",
+                "code_file": "pls-xgb.ipynb",
+                "language": "python",
+                "kernel_type": "notebook",
+                "enable_gpu": "true",
+                "enable_internet": "true",
+                "competition_sources": ["jane-street-market-prediction"]}
+
+    push_kernel(path, metadata)
+    return
+
+
+def push_tuner(path, user):
+    metadata = {"id": user + "/jane-street-tuner",
+                "title": "Jane Street - Tuner",
+                "code_file": "tuner.ipynb",
                 "language": "python",
                 "kernel_type": "notebook",
                 "enable_internet": "false",
-                "dataset_sources": [user + "/jane-street-data"],
-                "kernel_sources": [user + "/jane-street-tpu"]}
+                "kernel_sources": [user + "/jane-street-nn",
+                                   user + "/jane-street-eda-xgb",
+                                   user + "/jane-street-pls-xgb"],
+                "competition_sources": ["jane-street-market-prediction"]}
 
     push_kernel(path, metadata)
     return
 
 
-def push_submission_kernel(path, user):
+def push_submission(path, user):
     metadata = {"id": user + "/jane-street-submission",
                 "title": "Jane Street - Submission",
                 "code_file": "submission.ipynb",
@@ -79,62 +111,13 @@ def push_submission_kernel(path, user):
                 "kernel_type": "notebook",
                 "enable_internet": "false",
                 "enable_gpu": "true",
-                "kernel_sources": [user + "/jane-street-tpu",
-                                   user + "/jane-street-utility"],
-                "dataset_sources": [user + "/jane-street-data"],
+                "kernel_sources": [user + "/jane-street-nn",
+                                   user + "/jane-street-eda-xgb",
+                                   user + "/jane-street-pls-xgb",
+                                   user + "/jane-street-tuner"],
                 "competition_sources": ["jane-street-market-prediction"]}
 
     push_kernel(path, metadata)
-    return
-
-
-def push_eda_kernel(path, user):
-    metadata = {"id": user + "/jane-street-eda",
-                "title": "Jane Street - EDA",
-                "code_file": "eda.ipynb",
-                "language": "python",
-                "kernel_type": "notebook",
-                "enable_internet": "true",
-                "competition_sources": ["jane-street-market-prediction"]}
-
-    push_kernel(path, metadata)
-    return
-
-
-def kernel_to_dataset(user, kernel, metadata):
-    with tempfile.TemporaryDirectory() as tempdir:
-        # retrive the kernel output
-        generation = user + "/" + kernel
-        cmd = "kaggle kernels output " + generation + " -p " + tempdir
-        subprocess.run(cmd.split())
-
-        # remove the log file
-        os.remove(os.path.join(tempdir, kernel + ".log"))
-
-        # write the metadata into appropriate json file
-        metafile = os.path.join(tempdir, "dataset-metadata.json")
-        with open(metafile, "w") as file:
-            json.dump(metadata, file)
-
-        # create dataset using the kaggle command line tool
-        cmd = "kaggle datasets create -p " + tempdir
-        subprocess.run(cmd.split())
-
-    return
-
-
-def wait_for_kernel(user, slug, interval=300):
-    cmd = "kaggle kernels status " + user + "/" + slug
-    output = ""
-
-    # while the status of the kernel is not complete,
-    # wait and then check the status of the kernel
-    while '"complete"' not in output:
-        print(f"Waiting for {interval} seconds...")
-        time.sleep(interval)
-        output = subprocess.check_output(cmd.split()).decode()
-        print(output, end="")
-
     return
 
 
@@ -151,6 +134,21 @@ def push_kernel(path, metadata):
 
     # remove metadata file
     os.remove(metafile)
+    return
+
+
+def wait_for_kernel(user, slug, interval=300):
+    cmd = "kaggle kernels status " + user + "/" + slug
+    output = ""
+
+    # while the status of the kernel is not complete,
+    # wait and then check the status of the kernel
+    while "complete" not in output:
+        print(f"Waiting for {interval} seconds...")
+        time.sleep(interval)
+        output = subprocess.check_output(cmd.split()).decode()
+        print(output, end="")
+
     return
 
 
